86468863a86fee51244c71a9e40215d38c797ead
Vatsal <vatsalmanvar@gmail.com>
2023-04-26 18:33:32 +0530

    Component of Search and select email created

--- frontend/src/components/CreateProject.js
+++ frontend/src/components/CreateProject.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState, useContext} from 'react'
 import projectContext from '../context/project/projectContext';
+import SearchAndSelect from './SearchAndSelect';
 
 const CreateProject = () => {
 
@@ -7,29 +8,7 @@ const CreateProject = () => {
     const {users, fetchUsers} = context;
     const initialAdmin = [];
     const [admin, setAdmin] = useState(initialAdmin)
-    const initialSearchInput = "";
-    const [searchInput, setSearchInput] = useState(initialSearchInput)
-    const [searchResults, setSearchResults] = useState([])
-
-    const handleSearchChange = (e)=>{
-      //console.log("HandleOnChange ran")
-      setSearchInput(e.target.value)
-      if(!e.target.value) return setSearchResults([]);
-      const resultsArray = users.filter(users => ( !admin.includes(users.email) && users.email.includes(e.target.value)))
-      //console.log(e.target.value, resultsArray)
-      setSearchResults(resultsArray);
-    }
-
-    const handleOnClickOnSearch = (e) => {
-      console.log("Button pressed");
-      if(!admin.includes(e.target.value)) setAdmin(admin.concat(e.target.value));
-      setSearchInput(initialSearchInput);
-      setSearchResults([]);
-    }
-
-    const hadleRemoveFromAdmin = (e) => {
-      setAdmin(admin.filter(it => it!==e.target.value))
-    }
+    const [developer, setDeveloper] = useState(initialAdmin)
 
     useEffect(() => {
       if(users.length === 0) fetchUsers();
@@ -39,38 +18,9 @@ const CreateProject = () => {
   return (
     <div>
         <div className="container">
-        
-            <div className="mb-3">
-              <label htmlFor="searchInput" className="form-label">Search Email</label>
-              <input type="text" placeholder="" className="form-control" value={searchInput}  id="searchInput" name="searchInput" onChange={handleSearchChange} />
-            
-              {
-                searchResults.map((it, index)=>{
-                  return(
-                    <button key={index} className="list-group-item list-group-item-action" value={it.email} onClick={handleOnClickOnSearch}>{it.email}</button>
-                  )
-                })
-              }
-            </div>
-
-            <h3>ADMINS</h3>
-            {
-              admin.length===0
-              ?
-              <h5>No admin added</h5>
-              :
-              Object.values(admin).map((it, index)=>{
-                return(
-                  <>
-                    <br/>
-                    <button key={it} value={it} className="fa-sharp fa-solid fa-user-minus" onClick={hadleRemoveFromAdmin}></button>
-                    <span key={index} className="badge text-bg-dark mx-1">{it}</span>
-                  </>
-                )
-              })
-            }
+            <SearchAndSelect nameOfArray={"ADMIN"} buildArray={admin} setBuildArray={setAdmin} />
+            <SearchAndSelect nameOfArray={"DEVELOPER"} buildArray={developer} setBuildArray={setDeveloper} />
         </div>
-
     </div>
   )
 }
---
+++ frontend/src/components/SearchAndSelect.js
@@ -0,0 +1,77 @@
+import React, {useEffect, useState, useContext} from 'react'
+import projectContext from '../context/project/projectContext';
+
+const SearchAndSelect = (props) => {
+    const context = useContext(projectContext);
+    const {users, fetchUsers} = context;
+
+    const { nameOfArray, buildArray, setBuildArray} = props
+
+    const initialSearchInput = "";
+    const [searchInput, setSearchInput] = useState(initialSearchInput)
+    const [searchResults, setSearchResults] = useState([])
+
+    const handleSearchChange = (e)=>{
+      //console.log("HandleOnChange ran")
+      setSearchInput(e.target.value)
+      if(!e.target.value) return setSearchResults([]);
+      const resultsArray = users.filter(users => ( !buildArray.includes(users.email) && users.email.includes(e.target.value)))
+      //console.log(e.target.value, resultsArray)
+      setSearchResults(resultsArray);
+    }
+
+    const handleOnClickOnSearch = (e) => {
+      console.log("Button pressed");
+      if(!buildArray.includes(e.target.value)) setBuildArray(buildArray.concat(e.target.value));
+      setSearchInput(initialSearchInput);
+      setSearchResults([]);
+    }
+
+    const hadleRemoveFromAdmin = (e) => {
+      setBuildArray(buildArray.filter(it => it!==e.target.value))
+    }
+
+    useEffect(() => {
+        if(users.length === 0) fetchUsers();
+        // eslint-disable-next-line
+    }, [])
+    
+  return (
+    <div>
+        <div className="container">
+        
+        <div className="mb-3">
+          <label htmlFor="searchInput" className="form-label">Search Email</label>
+          <input type="text" placeholder="" className="form-control" value={searchInput}  id="searchInput" name="searchInput" onChange={handleSearchChange} />
+        
+          {
+            searchResults.map((it, index)=>{
+              return(
+                <button key={index} className="list-group-item list-group-item-action" value={it.email} onClick={handleOnClickOnSearch}>{it.email}</button>
+              )
+            })
+          }
+        </div>
+
+        <h3>{nameOfArray}</h3>
+        {
+          buildArray.length===0
+          ?
+          <h5>No {nameOfArray} added</h5>
+          :
+          Object.values(buildArray).map((it, index)=>{
+            return(
+              <>
+                <br/>
+                <button key={it} value={it} className="fa-sharp fa-solid fa-user-minus" onClick={hadleRemoveFromAdmin}></button>
+                <span key={index} className="badge text-bg-dark mx-1">{it}</span>
+              </>
+            )
+          })
+        }
+    </div>
+    </div>
+  )
+}
+
+export default SearchAndSelect
