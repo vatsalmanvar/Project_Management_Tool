cd1b2639a757eba0dbdb18eb44961fdf2c5c837c
Vatsal <vatsalmanvar@gmail.com>
2023-05-02 16:58:26 +0530

    Mongo local to Mongo Cloud

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/ProjectManagementTool?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
+const mongoURI = 'mongodb+srv://vatsalmanvar:vatsalmanvar@cluster0.ytkoko1.mongodb.net/test'
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI)
 }
 
 module.exports = connectToMongo;
+
+
+// mongo cloud URI: 'mongodb+srv://vatsalmanvar:vatsalmanvar@cluster0.ytkoko1.mongodb.net/test'
+// const mongoURI = 'mongodb://localhost:27017/ProjectManagementTool?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
